337f1ee2009cbb7dddad556d0fdbf655a2f762f7
Anderbone <yanchiyu@outlook.com>
2024-10-24 22:02:49 +0100

    before db model

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,4 +1,5 @@
 import type { MetaFunction } from "@remix-run/node";
+import { Button } from "@/components/ui/button";
 
 export const meta: MetaFunction = () => {
   return [
@@ -10,7 +11,7 @@ export const meta: MetaFunction = () => {
 export default function Index() {
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Welcome to Remix</h1>
+      <h1 className="text-3xl font-bold underline">Welcome to Remix</h1>
       <ul>
         <li>
           <a
@@ -36,6 +37,10 @@ export default function Index() {
           </a>
         </li>
       </ul>
+
+      <div>
+        <Button>Click me</Button>
+      </div>
     </div>
   );
 }
